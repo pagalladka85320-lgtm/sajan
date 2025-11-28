cf598bad1940213c2bb44ac3bc4948e233e15ab9
pagalladka85320-lgtm <pagalladka85320@gmail.com>
2025-11-28 16:32:15 +0530

    Update 00ma.js

--- Priyansh/commands/00ma.js
+++ Priyansh/commands/00ma.js
@@ -15,7 +15,7 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("ma") ||
+	if(react.includes("vaishnavi") ||
      react.includes("devi") || react.includes("MATa") || react.includes("MAtA") ||
 react.includes("MA") ||
 react.includes("MATA")) {
