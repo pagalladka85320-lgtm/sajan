7fcfb388a7e725a6f7aa185b490f8eea3b7f32d0
pagalladka85320-lgtm <pagalladka85320@gmail.com>
2025-11-28 15:47:38 +0530

    Update 00khana.js

--- Priyansh/commands/00khana.js
+++ Priyansh/commands/00khana.js
@@ -20,7 +20,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("kHana") ||
 react.includes("khAna")) {
 		var msg = {
-				body: `★━━━━━━━━━━━━━★🌶️𝐊𝐡𝐚 𝐋𝐨 𝐌𝐞𝐫𝐢 𝐉𝐚𝐚𝐧🌶️ ★━━━━━━━━━━━━━★`,attachment: fs.createReadStream(__dirname + `/noprefix/khana.gif`)
+				body: `★━━━━━━━━━━━━━★🌶️𝐊𝐡𝐚 𝐋𝐨 𝐌𝐞𝐫𝐢 𝐉𝐚𝐚𝐧🌶️ ★━━━━━━━━━━━━━★`,attachment: fs.createReadStream(__dirname + `/noprefix/khana.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍲", event.messageID, (err) => {}, true)
